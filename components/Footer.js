e4714aa8b0a3789358b7af7612720b3462f71836
stupendous-web <topher@stupendousweb.com>
2022-08-20 14:40:29 -0700

    Think twice, code once.

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <>
+      <div className={"uk-section uk-section-xsmall"}>
+        <div className={"uk-container uk-container-xsmall"}>
+          &copy; Copyright{" "}
+          <a
+            href={"https://stupendousweb.com"}
+            title={"Web App Development Services"}
+          >
+            Stupendous Web
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -11,58 +12,56 @@ export default function Home() {
       </Head>
       <div
         className={
-          "uk-width-1-1 uk-section uk-flex uk-flex-center uk-flex-middle uk-background-cover"
+          "uk-width-1-1 uk-section uk-section-xlarge uk-flex uk-flex-center uk-flex-middle uk-background-cover"
         }
         style={{
           backgroundImage: "url('/images/splash.jpg');",
         }}
-        data-uk-height-viewport={""}
       >
-        <div className={"uk-container uk-container-large"}>
-          <div
-            className={"uk-child-width-1-2@s uk-flex-middle"}
-            data-uk-grid={""}
+        <div className={"uk-container uk-container-xsmall"}>
+          <h1 className={"uk-heading-medium"}>JOCKEY COCK</h1>
+          <p className={"uk-text-lead"}>
+            Creating a FREE community of sex positive gay men through media,
+            listings, and social connections
+          </p>
+          <a
+            className={"uk-button uk-button-primary uk-button-large"}
+            onClick={() =>
+              alert(
+                "Early registration is coming soon! In the meantime, give me feedback!"
+              )
+            }
           >
-            <div>
-              <h1 className={"uk-heading-medium"}>JOCKEY COCK</h1>
-              <a
-                className={"uk-button uk-button-primary uk-button-large"}
-                onClick={() =>
-                  alert(
-                    "Early registration is coming soon! In the meantime, give me feedback!"
-                  )
-                }
-              >
-                Start your Profile
-              </a>
-            </div>
-            <div>
-              <p className={"uk-text-lead"}>
-                Creating a FREE community of sex positive gay men through media,
-                listings, and social connections
-              </p>
-              <p>
-                Create a profile, post to the social media feed, check out gay
-                listings, hookup, and make friends around you.
-              </p>
-              <h2>Make this Space Yours</h2>
-              <p>
-                It&apos;s complicated. Have a feature in mind or don&apos;t feel
-                comfortable as part of our community? Email{" "}
-                <a href={"mailto:topher@stupendousweb.com"}>
-                  topher@stupendousweb.com
-                </a>{" "}
-                and I&apos;ll do my best to help you have fun and feel included!
-              </p>
-              <h2>Volunteer or Donate</h2>
-              <p>
-                Want to help see this free community grow? Email me to volunteer
-                or donate!
-              </p>
-            </div>
-          </div>
+            Start your Profile
+          </a>
+          <p>
+            Create a profile, share sexy media, check out gay listings, hookup,
+            and make friends.
+          </p>
         </div>
       </div>
+      <div className={"uk-section uk-section-xlarge"}>
+        <div className={"uk-container uk-container-xsmall"}>
+          <h2>Make this Space Yours</h2>
+          <p>
+            It&apos;s complicated. Have a feature in mind or don&apos;t feel
+            comfortable as part of our community? Email{" "}
+            <a href={"mailto:topher@stupendousweb.com"}>
+              topher@stupendousweb.com
+            </a>{" "}
+            and I&apos;ll do my best to help you have fun and feel included!
+          </p>
+          <h2>Volunteer, Donate, or Sponsor!</h2>
+          <p>
+            Want to help see this free community grow? Email{" "}
+            <a href={"mailto:topher@stupendousweb.com"}>
+              topher@stupendousweb.com
+            </a>{" "}
+            to volunteer or donate!
+          </p>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
